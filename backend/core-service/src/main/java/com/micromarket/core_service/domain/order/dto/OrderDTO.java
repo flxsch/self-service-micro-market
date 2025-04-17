caba68ef2da78fed4d7de20b18a2e6a3b3daaa33
Yaroslav Zhyvotovskyi <yaroslav.zhyvotovskyi@bucher-suter.com>
2025-04-17 14:02:47 +0300

    GH-5: fix dependency

--- backend/core-service/src/main/java/com/micromarket/core_service/domain/order/dto/OrderDTO.java
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/order/dto/OrderDTO.java
@@ -1,6 +1,5 @@
 package com.micromarket.core_service.domain.order.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.micromarket.common.generic.BaseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
