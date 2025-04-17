f29c9c612c6ee5f46b00c47dc7276a7d6662a49f
Boreas033 <jeanluc.bouvard@icloud.com>
2025-04-17 14:34:32 +0300

    GH-6: add product

---
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/product/ProductService.java
@@ -0,0 +1,6 @@
+package com.micromarket.core_service.domain.product;
+
+import com.micromarket.common.generic.ExtendedService;
+
+public interface ProductService extends ExtendedService<Product> {
+}
