831247a330d6788ae8ee84f13c1a3074a68cf5ca
Felix Schneider <felix.schneider@noseryoung.com>
2025-04-22 14:41:59 +0300

    GH-37: Added User to Order

--- backend/core-service/src/main/java/com/micromarket/core_service/domain/order/Order.java
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/order/Order.java
@@ -1,14 +1,20 @@
 package com.micromarket.core_service.domain.order;
 
 import com.micromarket.common.generic.ExtendedEntity;
+import com.micromarket.core_service.domain.product.Product;
+import com.micromarket.core_service.domain.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "order")
 public class Order extends ExtendedEntity {
-    String userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    User user;
 }
