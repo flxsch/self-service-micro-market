d780a296b006bb71b3397b7d6e631c2efc7d4ed3
Felix Schneider <felix.schneider@noseryoung.com>
2025-04-22 17:08:45 +0300

    GH-37: Updated relationships & tested bootRun (doesn't work yet)

--- backend/common/src/main/java/com/micromarket/common/generic/ExtendedEntity.java
+++ backend/common/src/main/java/com/micromarket/common/generic/ExtendedEntity.java
@@ -1,45 +1,31 @@
 package com.micromarket.common.generic;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
+@Setter
 @Log4j2
 @MappedSuperclass
 public abstract class ExtendedEntity {
     @Id
+    @GeneratedValue(generator = "UUID")
     @Column(name = "id", updatable = false, nullable = false)
     private String id;
 
-    @Setter
+    @Column
     private String createdByUserId;
 
-    @Setter
+    @Column
     private String updatedByUserId;
 
-    @Setter
     @JoinColumn(updatable = false)
     protected LocalDateTime createdAt;
 
-    @Setter
     @Column
     protected LocalDateTime updatedAt;
-    protected ExtendedEntity() {
-        this.id = UUID.randomUUID().toString();
-    }
-    public ExtendedEntity(String id) {
-       this.id = id;
-    }
-    public ExtendedEntity setId(String id) {
-        this.id = id;
-        return this;
-    }
 }
