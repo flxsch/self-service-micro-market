1d783172477ea54371ef1c5907f89b55469c7517
Jérome Townsend <jerome.townsend@bucher-suter.com>
2025-04-25 14:22:57 +0300

    GH-42: Add get user by ID endpoint

--- backend/core-service/src/main/java/com/micromarket/core_service/domain/user/UserController.java
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/user/UserController.java
@@ -1,9 +1,12 @@
 package com.micromarket.core_service.domain.user;
 
-
+import com.micromarket.core_service.domain.user.dto.UserDTO;
+import com.micromarket.core_service.domain.user.dto.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public UserController(UserService userService) {
+    @Autowired
+    public UserController(final UserService userService, final UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
-    @GetMapping({"", "/"})
-    public ResponseEntity<String> getHelloWorld() {
-        return new ResponseEntity<String>("Hello world", HttpStatus.OK);
+    @GetMapping({"/{id}", "/{id}/"})
+    public ResponseEntity<UserDTO> getById(@PathVariable final String id) {
+        User user = userService.findById(id);
+        return new ResponseEntity<>(userMapper.toDTO(user), HttpStatus.OK);
     }
 }
