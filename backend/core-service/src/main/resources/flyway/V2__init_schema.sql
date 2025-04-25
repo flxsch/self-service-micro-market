173ee3ea97d566eceac26637218760393bd2c0f1
Felix Schneider <felix.schneider@noseryoung.com>
2025-04-25 14:49:25 +0300

    GH-41: Updated SQL statements and entity annotations

--- backend/core-service/src/main/resources/flyway/V2__init_schema.sql
+++ backend/core-service/src/main/resources/flyway/V2__init_schema.sql
@@ -1,39 +1,39 @@
 -- Users Table
 CREATE TABLE users (
-                       id varchar(255) PRIMARY KEY,
+                       id varchar(255) PRIMARY KEY NOT NULL,
                        first_name VARCHAR(100) NOT NULL,
                        last_name VARCHAR(100) NOT NULL,
                        email VARCHAR(255) UNIQUE NOT NULL,
                        birth_date DATE,
                        language VARCHAR(50),
-                       created_by_user_id VARCHAR(255),
+                       created_by_user_id VARCHAR(255) NOT NULL,
                        updated_by_user_id VARCHAR(255),
-                       created_at TIMESTAMP,
+                       created_at TIMESTAMP NOT NULL,
                        updated_at TIMESTAMP
 );
 
 -- Product Table
 CREATE TABLE product (
-                         id varchar(255) PRIMARY KEY,
+                         id varchar(255) PRIMARY KEY NOT NULL,
                          name VARCHAR(255) NOT NULL,
                          category VARCHAR(100) NOT NULL,
                          stock INT NOT NULL,
                          price DECIMAL(10, 2) NOT NULL,
                          barcode VARCHAR(255) NOT NULL,
-                         created_by_user_id VARCHAR(255),
+                         created_by_user_id VARCHAR(255) NOT NULL,
                          updated_by_user_id VARCHAR(255),
-                         created_at TIMESTAMP,
+                         created_at TIMESTAMP NOT NULL,
                          updated_at TIMESTAMP
 );
 
 -- Orders Table
 CREATE TABLE orders (
-                        id varchar(255) PRIMARY KEY,
+                        id varchar(255) PRIMARY KEY NOT NULL,
                         user_id varchar(255),
                         payment_id VARCHAR(255),
-                        created_by_user_id VARCHAR(255),
+                        created_by_user_id VARCHAR(255) NOT NULL,
                         updated_by_user_id VARCHAR(255),
-                        created_at TIMESTAMP,
+                        created_at TIMESTAMP NOT NULL,
                         updated_at TIMESTAMP,
                         FOREIGN KEY (user_id) REFERENCES users(id)
 );
