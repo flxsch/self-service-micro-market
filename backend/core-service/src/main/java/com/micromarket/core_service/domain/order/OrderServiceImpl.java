864355776b984fe7e7f0d094fe3b4a2d32060ce6
Boreas033 <jeanluc.bouvard@icloud.com>
2025-05-02 09:12:52 +0300

    GH-41: add missing end points

--- backend/core-service/src/main/java/com/micromarket/core_service/domain/order/OrderServiceImpl.java
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/order/OrderServiceImpl.java
@@ -1,8 +1,11 @@
 package com.micromarket.core_service.domain.order;
 
 import com.micromarket.common.generic.ExtendedServiceImpl;
+import com.micromarket.core_service.domain.product.Product;
 import org.springframework.stereotype.Service;
+import com.micromarket.core_service.domain.orderproduct.OrderProduct;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -15,4 +18,14 @@ public class OrderServiceImpl extends ExtendedServiceImpl<Order> implements Orde
     public Order findByPaymentId(String paymentId) {
         return ((OrderRepository) repository).findByPaymentId(paymentId).orElseThrow(() -> new NoSuchElementException((String.format("Order with payment id: '%s' doesn't exist", paymentId))));
     }
+
+    @Override
+    public List<Product> findProductsByOrderId(String orderId) {
+        Order order = findById(orderId);
+        return order.getOrderProducts().stream()
+                .map(OrderProduct::getProduct)
+                .toList();
+    }
+
+
 }
--- backend/core-service/src/main/java/com/micromarket/core_service/domain/orderproduct/OrderProduct.java
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/orderproduct/OrderProduct.java
@@ -3,8 +3,12 @@ package com.micromarket.core_service.domain.orderproduct;
 import com.micromarket.core_service.domain.order.Order;
 import com.micromarket.core_service.domain.product.Product;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "order_product")
 public class OrderProduct {
     @Id
