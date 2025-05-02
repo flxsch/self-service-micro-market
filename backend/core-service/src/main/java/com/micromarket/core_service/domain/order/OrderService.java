82b7e484413959c835750d9c487c2655751b03d4
Boreas033 <jeanluc.bouvard@icloud.com>
2025-05-02 09:12:52 +0300

    GH-41: add missing end points

--- backend/core-service/src/main/java/com/micromarket/core_service/domain/order/OrderService.java
+++ backend/core-service/src/main/java/com/micromarket/core_service/domain/order/OrderService.java
@@ -1,7 +1,10 @@
 package com.micromarket.core_service.domain.order;
 
 import com.micromarket.common.generic.ExtendedService;
+import com.micromarket.core_service.domain.product.Product;
+import java.util.List;
 
 public interface OrderService extends ExtendedService<Order> {
     Order findByPaymentId(String paymentId);
+    List<Product> findProductsByOrderId(String orderId);
 }
